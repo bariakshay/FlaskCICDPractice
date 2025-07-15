9be0e99f240adba1f5359bc64b5066ffa1e79bf9
bariakshay <bari_akshay@yahoo.in>
2025-07-15 10:44:16 +0530

    changes push

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Vaishnavi, thid id the cicd pipeline"
+    return "Hello, Vaishnavi, thid id the cicd pipeline V3.0"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
